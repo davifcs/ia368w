88f8b7784653d6421eade0de633f7b022adc9374
ia368w <davi.fcs@gmail.com>
2020-05-12 20:57:53 -0300

    Working version, needs variables validation

--- tarefa_2.py
+++ tarefa_2.py
@@ -7,10 +7,10 @@ host = 'http://127.0.0.1:4950'
 restthru.http_init()
 
 Ks = 0.05
-b = 165
-sigma_x = 0.5
-sigma_y = 0.5
-sigma_th = 0.03
+axis = 165
+R_sigma_x = 0.5
+R_sigma_y = 0.5
+R_sigma_th = 0.03
 timespan = 1
 
 def calculateEllipse(x,y,a,b,angle,steps=36):
@@ -51,7 +51,7 @@ def getVel():
 
     return l,r
   
-R = np.matrix([[sigma_x,0,0],[0,sigma_y,0],[0,0,sigma_th]])
+R = np.matrix([[R_sigma_x,0,0],[0,R_sigma_y,0],[0,0,R_sigma_th]])
 
 l_old = 0
 r_old = 0
@@ -80,13 +80,21 @@ while(i < 100):
     delta_l = l - l_old
     delta_r = r - r_old
 
-    delta_th = (delta_l + delta_r)/(2*b)*np.pi/180
+    #delta_th = (delta_r + delta_l)/(2*axis)
+    delta_th = (delta_r - delta_l)/(axis)
+
+    if delta_th > np.pi:
+        delta_th = delta_th-2*np.pi
+    elif delta_th < - np.pi:
+        delta_th = delta_th+2*np.pi
+
+    print(delta_th)
 
     delta_s = (delta_l + delta_r)/2 
 
     nabla_p_f = np.matrix([[1, 0, -delta_s*np.sin(th_kinematic+delta_th/2)], [0, 1, delta_s*np.cos(th_kinematic+delta_th/2)], [0, 0, 1]])
 
-    nabla_s_f = np.matrix([[1/2*np.cos(th_kinematic+delta_th/2) - delta_s/(4*b)*np.sin(th_kinematic+delta_th/2), 1/2*np.cos(th_kinematic+delta_th/2) + delta_s/(4*b)*np.sin(th_kinematic+delta_th/2)], [1/2*np.sin(th_kinematic+delta_th/2) + delta_s/(4*b)*np.cos(th_kinematic+delta_th/2), 1/2*np.sin(th_kinematic+delta_th/2) - delta_s/(4*b)*np.cos(th_kinematic+delta_th/2)], [1/(2*b), -1/(2*b)]])
+    nabla_s_f = np.matrix([[1/2*np.cos(th_kinematic+delta_th/2) - delta_s/(4*axis)*np.sin(th_kinematic+delta_th/2), 1/2*np.cos(th_kinematic+delta_th/2) + delta_s/(4*axis)*np.sin(th_kinematic+delta_th/2)], [1/2*np.sin(th_kinematic+delta_th/2) + delta_s/(4*axis)*np.cos(th_kinematic+delta_th/2), 1/2*np.sin(th_kinematic+delta_th/2) - delta_s/(4*axis)*np.sin(th_kinematic+delta_th/2)], [1/(2*axis), -1/(2*axis)]])
 
     sigma_delta_s = np.matrix([[Ks * np.abs(delta_r),0],[0,Ks * np.abs(delta_l)]])
 
@@ -104,28 +112,22 @@ while(i < 100):
     elif beta < - np.pi:
         beta = beta+2*np.pi
 
-    kinematic_next =  np.array([(delta_r+delta_l)/2*np.cos(th_kinematic+(delta_r-delta_l)/(4*b)),(delta_r+delta_l)/2*np.sin(th_kinematic+(delta_r-delta_l)/(4*b)),(delta_r-delta_l)/(2*b)])
+    kinematic_next =  np.array([(delta_r+delta_l)/2*np.cos(th_kinematic+(delta_r-delta_l)/(4*axis)),(delta_r+delta_l)/2*np.sin(th_kinematic+(delta_r-delta_l)/(4*axis)),(delta_r-delta_l)/(2*axis)])
     kinematic = kinematic + kinematic_next
 
     x_kinematic = kinematic[0]
     y_kinematic = kinematic[1]
     th_kinematic = kinematic[2]
 
-    # if th_kinematic > np.pi:
-    #     th_kinematic = th_kinematic-2*np.pi
-    # elif th_kinematic < - np.pi:
-    #     beth_kinematicta = th_kinematic+2*np.pi
 
-
-    # X,Y = calculateEllipse(x_kinematic,y_kinematic,a,b,beta)
+    X,Y = calculateEllipse(x_kinematic,y_kinematic,a,b,beta)
     ax.quiver(x_kinematic,y_kinematic,np.cos(th_kinematic),np.sin(th_kinematic))
     ax.scatter(x_kinematic,y_kinematic)
-    # ax.plot(X,Y)
+    ax.plot(X,Y)
 
-
-    # x_odometry,y_odometry,th_odometry = getPose()
-    # ax.quiver(x_odometry,y_odometry,np.cos(th_odometry),np.sin(th_odometry))
-    # ax.scatter(x_odometry,y_odometry)
+    x_odometry,y_odometry,th_odometry = getPose()
+    ax.quiver(x_odometry,y_odometry,np.cos(th_odometry),np.sin(th_odometry))
+    ax.scatter(x_odometry,y_odometry)
 
     plt.pause(timespan)
     plt.draw()
